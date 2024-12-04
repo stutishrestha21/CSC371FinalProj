324a15d291a25464d5ecb5a007f861b1917cf50c
Stuti Shrestha <stutishrestha21@augustana.edu>
2024-12-04 08:35:43 -0600

    greedy strategy

--- Project2RobbinTheHood/src/GreedyStrategy.java
+++ Project2RobbinTheHood/src/GreedyStrategy.java
@@ -1,5 +1,4 @@
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * A greedy algorithm to select vertices for robbing.
@@ -10,10 +9,50 @@ public class GreedyStrategy implements RobbingStrategy {
     public List<String> chooseOrderToAttack(LabeledValueGraph graph) {
 
         List<String> vertices = graph.getAllVertexLabels();
-        //TODO: Implement a greedy strategy to select the order of vertices to attack
+        Map<Integer, List<String>> valueGold = new TreeMap<>(Collections.reverseOrder());
+        Map<String, Integer> attackedMap = new LinkedHashMap<>();
+        Set<String> alertedSet = new LinkedHashSet<>();
 
-        return vertices;
+        // Reverse order sort
+        for (String vertex : vertices) {
+            int gold = graph.getValueAt(vertex);
+            valueGold.putIfAbsent(gold, new ArrayList<>());
+            valueGold.get(gold).add(vertex);
+        }
 
+        // Include # fort first in the map
+        for (String vertex : vertices) {
+            if (vertex.contains("#")) {
+                attackedMap.put(vertex, graph.getValueAt(vertex));
+                List<String> neighbors = graph.adj(vertex);
+            }
+        }
+
+        for (Map.Entry<Integer, List<String>> entry : valueGold.entrySet()) {
+            for (String vertex : entry.getValue()) {
+                if (attackedMap.containsKey(vertex)) continue;
+
+                int vertexValue = graph.getValueAt(vertex);
+
+                if (!vertex.contains("!") && !vertex.contains("*") && !alertedSet.contains(vertex)) {
+                    attackedMap.put(vertex, vertexValue);
+                    List<String> neighbors = graph.adj(vertex);
+                    for (String neighbor : neighbors) {
+                        if (!attackedMap.containsKey(neighbor)) {
+                            alertedSet.add(neighbor);
+                        }
+                    }
+                }
+            }
+        }
+
+        for (String newVertex:graph.getAllVertexLabels() ) {
+            if (!attackedMap.containsKey(newVertex)){
+                attackedMap.put(newVertex, graph.getValueAt(newVertex));
+            }
+        }
+
+
+        return new ArrayList<>(attackedMap.keySet());
     }
-
 }
